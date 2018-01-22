0de5d2b5b42769e29f26356b71a9200fa5c5b6e5
Brian Foo <brian@youaremyjoy.org>
2018-01-22 18:17:27 -0500

    implement basic current draw

---
+++ scripts/lib.py
@@ -0,0 +1,66 @@
+import math
+import numpy as np
+import numpy.ma as ma
+
+def clamp(value, low=0.0, high=1.0):
+    value = min(value, high)
+    value = max(value, low)
+    return value
+
+def distance(p0, p1):
+    return math.sqrt((p0[0] - p1[0])**2 + (p0[1] - p1[1])**2)
+
+def getLat(mu, r):
+    lat = lerp(r[0], r[1], mu)
+    return lat
+
+def getLon(mu, r):
+    lon = lerp(r[0], r[1], mu)
+    if lon >= 360:
+        lon = lon - 360
+    elif lon < 0:
+        lon = 360 + lon
+    lon = lon - 180
+    return lon
+
+def lerp(a, b, mu):
+    return (b-a) * mu + a
+
+def mean(data):
+    n = len(data)
+    if n < 1:
+        return 0
+    else:
+        return 1.0 * sum(data) / n
+
+def norm(value, a, b):
+    n = 1.0 * (value - a) / (b - a)
+    n = min(n, 1)
+    n = max(n, 0)
+    return n
+
+def normLat(lat, r):
+    return norm(lat, r[0], r[1])
+
+def normLon(lon, r):
+    if lon < r[0]:
+        lon += 360
+    return norm(lon, r[0], r[1])
+
+def uvDataAt(month, lng, lat, udata, vdata):
+    depth = 0
+    tLen = len(udata)
+    mus = []
+    mvs = []
+    for t in range(tLen):
+        tmonth = int(round(1.0 * t / (tLen-1) * 11.0))
+        if tmonth == month:
+            # retrieve velocity
+            u = udata[t][depth][lat][lng]
+            v = vdata[t][depth][lat][lng]
+            if not (np.isnan(u) or u is ma.masked or np.isnan(v) or v is ma.masked):
+                mus.append(u)
+                mvs.append(v)
+    u = mean(mus)
+    v = mean(mvs)
+    return (u, v)
--- scripts/processOceanCurrentData.py
+++ scripts/processOceanCurrentData.py
@@ -1,21 +1,39 @@
 # -*- coding: utf-8 -*-
 
+import argparse
 import json
+from lib import *
 import math
 from netCDF4 import Dataset
-import numpy as np
-import numpy.ma as ma
+from PIL import Image, ImageDraw
+from pprint import pprint
 import sys
 
-INPUT_FILE = "../data/downloaded/oscar_vel2016.nc" # Source: https://podaac.jpl.nasa.gov/dataset/OSCAR_L4_OC_third-deg
-OUTPUT_FILE = "../data/oscar_vel2016.json"
-PARTICLES_PER_ROW = 60
-PARTICLES_PER_COL = 24
+parser = argparse.ArgumentParser()
+# Source: https://podaac.jpl.nasa.gov/dataset/OSCAR_L4_OC_third-deg
+# Doc: ftp://podaac-ftp.jpl.nasa.gov/allData/oscar/preview/L4/oscar_third_deg/docs/oscarthirdguide.pdf
+parser.add_argument('-in', dest="INPUT_FILE", default="../data/downloaded/oscar_vel2016.nc", help="Input NetCDF data file")
+parser.add_argument('-out', dest="OUTPUT_FILE", default="../data/oscar_vel2016.json", help="Output json file")
+parser.add_argument('-ppr', dest="PARTICLES_PER_ROW", type=int, default=120, help="Particles per row")
+parser.add_argument('-ppc', dest="PARTICLES_PER_COL", type=int, default=60, help="Particles per col")
+parser.add_argument('-ppp', dest="POINTS_PER_PARTICLE", type=int, default=50, help="Points per particle")
+parser.add_argument('-vel', dest="VELOCITY_MULTIPLIER", type=float, default=1.0, help="Velocity mulitplier")
+parser.add_argument('-dt', dest="DISPLAY_THRESHOLD", type=float, default=0.333, help="Percentage of particles to show depending on curve distance")
+
+args = parser.parse_args()
+
+INPUT_FILE = args.INPUT_FILE
+OUTPUT_FILE = args.OUTPUT_FILE
+PARTICLES_PER_ROW = args.PARTICLES_PER_ROW
+PARTICLES_PER_COL = args.PARTICLES_PER_COL
+POINTS_PER_PARTICLE = args.POINTS_PER_PARTICLE
+VELOCITY_MULTIPLIER = args.VELOCITY_MULTIPLIER
+DISPLAY_THRESHOLD = args.DISPLAY_THRESHOLD
+
 PARTICLES = PARTICLES_PER_COL * PARTICLES_PER_ROW
-POINTS_PER_PARTICLE = 10
-WIDTH = 960
-HEIGHT = 480
-VELOCITY_MULTIPLIER = 1.0
+LAT_RANGE = (-80, 80) # latitude is represented from -80° to 80° with a 1/3° resolution
+LNG_RANGE = (20, 420) # longitude is represented from  as 20° to 420°
+PRECISION = 3
 
 ds = Dataset(INPUT_FILE, 'r')
 
@@ -24,36 +42,23 @@ us = ds.variables['u'][:]
 vs = ds.variables['v'][:]
 depth = 0
 
-timeCount = len(us) # this should be 72
+timeCount = len(us) # this should be 72, i.e. ~5 day interval
 lats = len(us[0][depth]) # this should be 1201
-lngs = len(us[0][depth][0]) # this should be 481
+lngs = len(us[0][depth][0]) # this should be 481;
 print "%s measurements found with %s degrees (lng) by %s degrees (lat)" % (timeCount, lngs, lats)
 
-def mean(data):
-    n = len(data)
-    if n < 1:
-        return 0
-    else:
-        return 1.0 * sum(data) / n
-
-def uvDataAt(month, lng, lat, udata, vdata):
-    depth = 0
-    tLen = len(udata)
-    mus = []
-    mvs = []
-    for t in range(tLen):
-        tmonth = int(round(1.0 * t / (tLen-1) * 11.0))
-        if tmonth == month:
-            # retrieve velocity
-            u = udata[t][depth][lat][lng]
-            v = vdata[t][depth][lat][lng]
-            if np.isnan(u) or u is ma.masked or np.isnan(v) or v is ma.masked:
-                continue
-            mus.append(u)
-            mvs.append(u)
-    u = mean(mus)
-    v = mean(mvs)
-    return (u, v)
+# draw month data
+# im = Image.new("RGB", (lngs, lats), (0, 0, 0))
+# pixels = im.load()
+# for lat in range(lats):
+#     for lng in range(lngs):
+#         u, v = uvDataAt(0, lng, lat, us, vs)
+#         mag = math.sqrt(u * u + v * v)
+#         normal = clamp(mag / 2.0)
+#         c = normal * 255.0
+#         c = int(round(c))
+#         pixels[lng, lat] = (c, c, c)
+# im.save("monthData.png")
 
 print "Target particles: %s" % PARTICLES
 data = []
@@ -61,66 +66,96 @@ data = []
 # go through each time interval
 for month in range(12):
 
+    print "Month %s" % (month+1)
     particles = []
+
+    # u, v = uvDataAt(month, 799, 131, us, vs)
+    # print "%s, %s" % (u, v)
+    # sys.exit(1)
 
     # init particles
     for col in range(PARTICLES_PER_COL):
         for row in range(PARTICLES_PER_ROW):
             yp = 1.0 * col / (PARTICLES_PER_COL-1)
             xp = 1.0 * row / (PARTICLES_PER_ROW-1)
-            lat = int(round(yp * (lats-1)))
-            lng = int(round(xp * (lngs-1)))
-            x = int(round(xp * (WIDTH-1)))
-            y = int(round(yp * (HEIGHT-1)))
-            i = y * WIDTH + x
-            curve = [i]
+            lng = getLon(xp, LNG_RANGE)
+            lat = getLat(yp, LAT_RANGE)
+            lng = round(lng, PRECISION)
+            lat = round(lat, PRECISION)
+            coordinates = [(lng, lat)]
+            cDistance = 0
 
-            for j in range(POINTS_PER_PARTICLE):
+            xp = normLon(lng, LNG_RANGE)
+            yp = normLat(lat, LAT_RANGE)
+            ui = int(round(xp * (lngs-1)))
+            vi = int(round(yp * (lats-1)))
+
+            for j in range(POINTS_PER_PARTICLE-1):
                 # retrieve velocity
-                u, v = uvDataAt(month, lng, lat, us, vs)
+                u, v = uvDataAt(month, ui, vi, us, vs)
 
                 # particle is standing still
                 if u == 0 and v == 0:
                     break
 
                 # move particle based on velocity
-                x += int(round(u * VELOCITY_MULTIPLIER))
-                y += int(round(v * VELOCITY_MULTIPLIER))
+                # mag = math.sqrt(u * u + v * v)
+                lng += u * VELOCITY_MULTIPLIER
+                lat += (-1.0 * v) * VELOCITY_MULTIPLIER
 
-                # check for bounds
-                if y < 0 or y >= HEIGHT:
+                # if lng < -180 or lng > 180:
+                #     break
+                if lat < LAT_RANGE[0] or lat > LAT_RANGE[1]:
                     break
-                if x < 0:
-                    x = WIDTH + x
-                if x > WIDTH - 1:
-                    x = x - (WIDTH-1)
 
-                # add point to curve
-                curve.append(y * WIDTH + x)
+                # add point
+                coordinates.append((lng, lat))
+                cDistance += distance(coordinates[j], (lng, lat))
 
                 # retrieve new lat/lng
-                yp = 1.0 * y / (HEIGHT-1)
-                xp = 1.0 * x / (WIDTH-1)
-                lat = int(round(yp * (lats-1)))
-                lng = int(round(xp * (lngs-1)))
+                xp = normLon(lng, LNG_RANGE)
+                yp = normLat(lat, LAT_RANGE)
+                ui = int(round(xp * (lngs-1)))
+                vi = int(round(yp * (lats-1)))
 
             # only add curves that move
-            if len(curve) > 1:
-                particles.append(curve)
+            if len(coordinates) > POINTS_PER_PARTICLE * 0.99:
+                particles.append({
+                    "points": coordinates,
+                    "distance": cDistance
+                })
 
-    print "Actual particles: %s" % len(particles)
+            sys.stdout.write('\r')
+            sys.stdout.write("%s%%" % round(1.0*(col * PARTICLES_PER_ROW + row)/PARTICLES*100,1))
+            sys.stdout.flush()
+
+    pLen = len(particles)
+    particles = sorted(particles, key=lambda k: k['distance'], reverse=True)
+    displayLen = int(round(pLen * DISPLAY_THRESHOLD))
+    particles = particles[:displayLen]
+    particles = [p["points"] for p in particles]
+
+    print "Actual particles: %s" % pLen
+    print "Display particles: %s" % displayLen
     data.append(particles)
 
-    sys.stdout.write('\r')
-    sys.stdout.write("%s%%" % round(1.0*month/11*100,1))
-    sys.stdout.flush()
+    # Draw sample image
+    # WIDTH = 1200
+    # HEIGHT = 600
+    # im = Image.new("RGB", (WIDTH, HEIGHT), (0, 0, 0))
+    # draw = ImageDraw.Draw(im)
+    # for coordinates in particles:
+    #     curve = [(norm(c[0], -180, 180) * WIDTH, norm(c[1], -90, 90) * HEIGHT) for c in coordinates]
+    #     draw.line(curve, fill=255)
+    # del draw
+    # im.save("monthDraw.png", "PNG")
+    # sys.exit(1)
+    # break
 
 jsonOut = {
     "yearInterval": timeCount,
     "lats": lats,
     "lngs": lngs,
-    "width": WIDTH,
-    "height": HEIGHT,
     "data": data
 }
 
